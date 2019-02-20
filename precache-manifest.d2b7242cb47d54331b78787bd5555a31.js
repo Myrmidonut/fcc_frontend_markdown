7ae7ff5e24c21c3f3909504799c64254e0528943
Frederik Funke <frederik-funke@gmx.de>
2019-02-20 17:30:17 +0100

    Updates

---
+++ precache-manifest.d2b7242cb47d54331b78787bd5555a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cecd27396d1300d8c0c",
+    "url": "/fcc_frontend_markdown/static/css/main.474b879d.chunk.css"
+  },
+  {
+    "revision": "7cecd27396d1300d8c0c",
+    "url": "/fcc_frontend_markdown/static/js/main.7cecd273.chunk.js"
+  },
+  {
+    "revision": "615ab952785e642bfaa4",
+    "url": "/fcc_frontend_markdown/static/js/runtime~main.615ab952.js"
+  },
+  {
+    "revision": "e055c6a9a1430993f267",
+    "url": "/fcc_frontend_markdown/static/js/2.e055c6a9.chunk.js"
+  },
+  {
+    "revision": "f01e776ccda1c389800209da39793d74",
+    "url": "/fcc_frontend_markdown/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fcc_frontend_markdown/precache-manifest.ffa3d48dd5e04e215c213700c2d4f51d.js"
+  "/fcc_frontend_markdown/precache-manifest.d2b7242cb47d54331b78787bd5555a31.js"
 );
 
 workbox.clientsClaim();
