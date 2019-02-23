de178b507321961b9f28eb3cb6f822e7181cb6e6
Frederik Funke <frederik-funke@gmx.de>
2019-02-23 19:48:23 +0100

    Updates

--- precache-manifest.17c785d6193544304300784e20abf05c.js
+++ precache-manifest.17c785d6193544304300784e20abf05c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "7cecd27396d1300d8c0c",
-    "url": "/fcc_frontend_markdown/static/css/main.474b879d.chunk.css"
+    "revision": "5cef9c760aa7eb79ec3c",
+    "url": "/fcc_frontend_markdown/static/css/main.7429b0c1.chunk.css"
   },
   {
-    "revision": "7cecd27396d1300d8c0c",
-    "url": "/fcc_frontend_markdown/static/js/main.7cecd273.chunk.js"
+    "revision": "5cef9c760aa7eb79ec3c",
+    "url": "/fcc_frontend_markdown/static/js/main.5cef9c76.chunk.js"
   },
   {
     "revision": "615ab952785e642bfaa4",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/fcc_frontend_markdown/static/js/2.9553c75f.chunk.js"
   },
   {
-    "revision": "5d6d1f126313efe3c5e134934fdb7894",
+    "revision": "6fc7f10e47eae0e49df5a5a82adc8b54",
     "url": "/fcc_frontend_markdown/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fcc_frontend_markdown/precache-manifest.f0d14b53ea40b2ff0f30bd5459ca7a42.js"
+  "/fcc_frontend_markdown/precache-manifest.17c785d6193544304300784e20abf05c.js"
 );
 
 workbox.clientsClaim();
